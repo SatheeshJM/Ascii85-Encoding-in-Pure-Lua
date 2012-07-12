6b97d1978f6fb4ec70251d13b9c1a13ea538ccb3
Satheesh Kumar <satheeshrulzz@gmail.com>
2012-07-12 23:49:30 +0530

    Initial Commit

--- main.lua
+++ main.lua
@@ -4,8 +4,6 @@ local ascii85 = require "ascii85"
 
 
 local input = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure."
-local input = "abcd\0\0\0\0dbca"
-
 
 local enc = ascii85.encode(input)
 print(enc)
